4426442d000cb10eebc81a655a625a811dc539dc
vasiliy <vaszol@mail.ru>
2021-08-08 23:55:54 +0300

    рисуем квадрат на фото

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 import cv2
 import numpy as np
 
-photo = np.zeros((300, 300, 3), dtype='uint8')
+photo = np.zeros((450, 450, 3), dtype='uint8')
 
-photo[:] = 255, 0, 0
+# RGB - стандарт
+# BGR - формат в OpenCV
+# photo[100:150, 200:280] = 119, 201, 105
+
+cv2.rectangle(photo, (0, 0), (100, 100), (119, 201, 105), thickness=3)
 
 cv2.imshow('Photo', photo)
 cv2.waitKey(0)
