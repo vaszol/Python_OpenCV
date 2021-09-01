4b9f8eccd18790a494d0814e31eab07072b283ee
vasiliy <vaszol@mail.ru>
2021-09-01 21:15:23 +0300

    Примеры трансформации видео

--- main.py
+++ main.py
@@ -1,19 +1,21 @@
 import cv2
 import numpy as np
 
-photo = np.zeros((450, 450, 3), dtype='uint8')
 
-# RGB - стандарт
-# BGR - формат в OpenCV
-# photo[100:150, 200:280] = 119, 201, 105
+cap = cv2.VideoCapture('videos/VID-20210803-WA0004.mp4')
 
-cv2.rectangle(photo, (50, 50), (100, 100), (119, 201, 105), thickness=3)
+while True:
+    success, img = cap.read()
 
-cv2.line(photo, (0, photo.shape[0] // 2), (photo.shape[1], photo.shape[0] // 2), (119, 201, 105), thickness=3)
+    # new_img = cv2.resize(img, (720, 1280))
+    img = cv2.GaussianBlur(img, (9,9), 0)
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    img = cv2.Canny(img, 100, 100)
+    kernel = np.ones((5, 5), np.uint8)
+    img = cv2.dilate(img, kernel, iterations=1)
+    img = cv2.erode(img, kernel, iterations=1)
 
-cv2.circle(photo, (photo.shape[1] // 2, photo.shape[0] // 2), 50, (119, 201, 105), thickness=cv2.FILLED)
+    cv2.imshow('Result', img)
 
-cv2.putText(photo, 'vaszol', (100, 150), cv2.FONT_HERSHEY_TRIPLEX, 1, (255, 0, 0), thickness=2)
-
-cv2.imshow('Photo', photo)
-cv2.waitKey(0)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
