7fd0acd5877f296838f392a5d1bf8ca01d100a8c
vasiliy <vaszol@mail.ru>
2021-09-01 22:54:02 +0300

    Распознавание лиц

--- main.py
+++ main.py
@@ -2,19 +2,15 @@ import cv2
 import numpy as np
 
 
-img = cv2.imread('images/2020-01-05_213603.jpg')
-new_img = np.zeros(img.shape, dtype='uint8')
+img = cv2.imread('images/faces_foto.jpg')
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-img = cv2.GaussianBlur(img, (5,5), 0)
+focus = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
-img = cv2.Canny(img, 100, 140)
+results = focus.detectMultiScale(gray, scaleFactor=2, minNeighbors=3)
 
-# получаем контуры изображения
-con, hir = cv2.findContours(img, cv2.RETR_LIST, cv2.CHAIN_APPROX_NONE)
+for (x,y,w,h) in results:
+    cv2.rectangle(img,(x,y,),(x+w,y+h),(0,0,255),thickness=3)
 
-# выводим полученные контуры
-cv2.drawContours(new_img, con, -1, (230,111,148),1)
-
-cv2.imshow("Result", new_img)
+cv2.imshow("Result", img)
 cv2.waitKey(0)
