5373cdb61b539135400e88679381330cdaeb8c1c
vasiliy <vaszol@mail.ru>
2021-09-01 21:30:17 +0300

    Контуры изображения

--- main.py
+++ main.py
@@ -1,31 +1,20 @@
 import cv2
 import numpy as np
 
+
 img = cv2.imread('images/2020-01-05_213603.jpg')
+new_img = np.zeros(img.shape, dtype='uint8')
 
-# зеркалирование
-img = cv2.flip(img, 1)
+img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+img = cv2.GaussianBlur(img, (5,5), 0)
 
+img = cv2.Canny(img, 100, 140)
 
-# вращение
-def rotate(img_param, angle):
-    height, width = img_param.shape[:2]
-    point = (width // 2, height // 2) # точка вращения
+# получаем контуры изображения
+con, hir = cv2.findContours(img, cv2.RETR_LIST, cv2.CHAIN_APPROX_NONE)
 
-    mat = cv2.getRotationMatrix2D(point, angle, 1)
-    return cv2.warpAffine(img_param, mat, (width, height))
+# выводим полученные контуры
+cv2.drawContours(new_img, con, -1, (230,111,148),1)
 
-
-# img = rotate(img, 90)
-
-# смещение
-def transfom(img_param,x,y):
-    mat = np.float32([[1,0,x],[0,1,y]])
-    return cv2.warpAffine(img_param, mat,(img_param.shape[1],img_param.shape[0]))
-
-
-img = transfom(img, 30, 200)
-
-
-cv2.imshow("Result", img)
+cv2.imshow("Result", new_img)
 cv2.waitKey(0)
