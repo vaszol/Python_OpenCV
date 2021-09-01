30c897161c517af1db3ebfcd5ff9be35e43d2101
vasiliy <vaszol@mail.ru>
2021-09-01 21:18:30 +0300

    Повороты и смещения

--- main.py
+++ main.py
@@ -1,21 +1,31 @@
 import cv2
 import numpy as np
 
+img = cv2.imread('images/2020-01-05_213603.jpg')
 
-cap = cv2.VideoCapture('videos/VID-20210803-WA0004.mp4')
+# зеркалирование
+img = cv2.flip(img, 1)
 
-while True:
-    success, img = cap.read()
 
-    # new_img = cv2.resize(img, (720, 1280))
-    img = cv2.GaussianBlur(img, (9,9), 0)
-    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    img = cv2.Canny(img, 100, 100)
-    kernel = np.ones((5, 5), np.uint8)
-    img = cv2.dilate(img, kernel, iterations=1)
-    img = cv2.erode(img, kernel, iterations=1)
+# вращение
+def rotate(img_param, angle):
+    height, width = img_param.shape[:2]
+    point = (width // 2, height // 2) # точка вращения
 
-    cv2.imshow('Result', img)
+    mat = cv2.getRotationMatrix2D(point, angle, 1)
+    return cv2.warpAffine(img_param, mat, (width, height))
 
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+
+# img = rotate(img, 90)
+
+# смещение
+def transfom(img_param,x,y):
+    mat = np.float32([[1,0,x],[0,1,y]])
+    return cv2.warpAffine(img_param, mat,(img_param.shape[1],img_param.shape[0]))
+
+
+img = transfom(img, 30, 200)
+
+
+cv2.imshow("Result", img)
+cv2.waitKey(0)
